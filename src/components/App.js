a7f7c279d2690df4884670eb886498e5ac959f69
arshad012 <forarshad99@gmail.com>
2024-12-18 18:55:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,26 @@
 import React from "react";
 import './../styles/App.css';
 
+function WeatherDisplay(props) {
+    const {temperature, conditions} = props;
+
+    return temperature ? <div style={{color: temperature > 20 ? 'red' : 'blue'}}>
+        <p>Temperature is {temperature}</p>
+        <p>Condition is {conditions}</p>
+    </div> : <></>
+}
+
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+  const [weatherState, setWeatherState] = useState({ temperature: 25, conditions: "Sunny" });
+    
+    useEffect(() => {
+        WeatherDisplay(weatherState);
+    }, [])
+    
+
+    return <>
+        <WeatherDisplay temperature={weatherState.temperature} conditions={weatherState.conditions} />
+    </>
 }
 
 export default App
