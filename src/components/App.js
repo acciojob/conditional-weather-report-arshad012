607e66838e15a22e1711c20efda4676e0b7942b4
arshad012 <forarshad99@gmail.com>
2024-12-20 15:10:22 +0530

    main

--- src/components/App.js
+++ src/components/App.js
@@ -6,16 +6,17 @@ function WeatherDisplay({temperature, conditions}) {
 
     return <div>
         <p>Temperature: <span style={{color: temperature > 20 ? 'red' : 'blue'}}>{temperature}</span></p>
-        <p>Condition: <span style={{color: temperature > 20 ? 'red' : 'blue'}}>{conditions}</span></p>
+        <p>Conditions: {conditions}</p>
     </div>
 }
 
 const App = () => {
   const [weatherState, setWeatherState] = useState({ temperature: 25, conditions: "Sunny" });
     
-    // useEffect(() => {
-    //     WeatherDisplay(weatherState);
-    // }, [])
+    useEffect(() => {
+        const updatedWeatherState = weatherState;
+        setWeatherState(updatedWeatherState);
+    }, [])
     
 
     return <>
